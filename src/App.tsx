781267ca7929023e5542df6583eb70c8b99a0bc5
PauloFonseca <pfonseca.arq@gmail.com>
2024-01-29 17:37:11 -0300

    Faça com que o próximo livro da lista seja exibido ao clicar no botão Próximo livro.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
+import { useState } from 'react';
 import './App.css';
 import Book from './components/Book';
 import BookList from './components/BookList';
 import data from './data.json';
+import { BookInfoType } from './types/index';
 
 const bookIndexStart = 10;
 const bookIndexEnd = 15;
 const placeHolderList = data.slice(bookIndexStart, bookIndexEnd); // esse código deverá ser excluído após a implementação do requisito 2
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [list, setList] = useState(' ...');
+  const [wishList, setWishList] = useState<BookInfoType[]>([]);
+  const [isWishList, setIsWishList] = useState(false);
+
+  const handleWishList = () => {
+    setWishList([...wishList, data[count]]);
+  };
+
+  const handleNextPage = () => {
+    if (count + 1 < data.length) {
+      setCount(count + 1);
+    } else {
+      setCount(0);
+    }
+  };
+
   return (
     <div className="app">
       <div className="book-selector">
-        <Book bookInfo={ data[0] } showDetails />
+        <Book bookInfo={ data[count] } showDetails />
         <div className="selector-buttons">
-          <button>Adicionar à lista de desejos</button>
+          <button onClick={ handleWishList }>Adicionar à lista de desejos</button>
           <button>Adicionar à lista de leitura</button>
           <button>Adicionar à lista de lidos</button>
-          <button>Próximo livro</button>
+          <button onClick={ handleNextPage }>Próximo livro</button>
         </div>
       </div>
 
@@ -25,8 +44,11 @@ function App() {
         <button>Exibir lista de leitura</button>
         <button>Exibir lista de lidos</button>
       </div>
-      <h1>Lista de ...</h1>
-      <BookList books={ placeHolderList } />
+      <h1>
+        Lista de
+        {list}
+      </h1>
+      <BookList books={ wishList } />
     </div>
   );
 }
